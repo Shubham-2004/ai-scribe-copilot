c42c5e4f8fb3e4ad87ccd534669e83d7598a5f7a
Shubham-2004 <shubhambera2004@gmail.com>
2025-09-28 14:32:08 +0530

    add: deploy the backend in render

--- stream_script_app/lib/features/auth/services/auth_service.dart
+++ stream_script_app/lib/features/auth/services/auth_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-	static const String baseUrl = 'http://192.168.1.7:3000/auth';
+	static const String baseUrl = 'https://streamscript.onrender.com/auth';
 
 	Future<Map<String, dynamic>> signup(String email, String password) async {
 		final response = await http.post(
